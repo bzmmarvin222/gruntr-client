af3ddc9838b81c2a013d53c1562348f42ad3b127
bzmmarvin222 <bzmmarvin222@gmail.com>
2018-08-08 20:37:43 +0200

    added splashscreen, added icon to toolbar, app now visible for other devices in network via start

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainToolbarComponent } from './main-toolbar/main-toolbar.component';
 import {MatToolbarModule} from "@angular/material";
+import { SplashScreenComponent } from './splash-screen/splash-screen.component';
 
 @NgModule({
   imports: [
     CommonModule,
     MatToolbarModule
   ],
-  declarations: [MainToolbarComponent],
+  declarations: [MainToolbarComponent, SplashScreenComponent],
   exports: [
-    MainToolbarComponent
+    MainToolbarComponent,
+    SplashScreenComponent
   ]
 })
 export class CoreModule { }
